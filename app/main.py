df42ed2ca3eca43783f0b79b438f883ab2c58cd6
ba2slk <0730bss@gmail.com>
2025-12-29 01:50:59 +0900

    feat: 베이스라인 RAG 서버 구현 완료

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.rag.api.router import router as chat_router
+from app.rag.dependencies import get_vector_repository
+
+# Meilisearch 설정 (서버 가동 시점에 최초 1회 실행)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        repo = get_vector_repository()
+        if hasattr(repo, 'initialize'):
+            repo.initialize()
+    except Exception as e:
+        print(f"Falied to connect to Meilisearch. {e}" )
+        
+    yield
+
+
+app = FastAPI(
+    title="CatchUp RAG Server",
+    lifespan=lifespan
+)
+
+app.include_router(chat_router)
+
+
+# 헬스 체크
+@app.get("/")
+async def health_chech():
+    return {"status": "ok", "message": "RAG Server is running."}
